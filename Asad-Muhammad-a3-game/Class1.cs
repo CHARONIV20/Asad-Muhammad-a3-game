56defb93bda25f469a25877e9910ae0e29aa5855
Muhammad Asad <000880751@mohawkcollege.ca>
2024-06-03 10:50:28 -0400

    Add Visual Studio project

---
+++ Asad-Muhammad-a3-game/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+public class Player
+{
+    Vector2 position;
+
+    public Player()
+    {
+        // Place player in center of screen
+        position.X = Raylib.GetScreenWidth() / 2;
+        position.Y = Raylib.GetScreenHeight() / 2;
+    }
+
+    public void Draw()
+    {
+        float headRadius = 22.5f;
+        float width = 40;
+        float height = 100;
+        Vector2 legbase = position + new Vector2(0, -30);
+        Vector2 footleft = position + new Vector2(-width / 2, 0);
+        Vector2 footRight = position + new Vector2(+width / 2, 0);
+        Vector2 armBase = position + new Vector2(0, -40);
+        Vector2 handleft = armBase + new Vector2(-width / 2, +5);
+        Vector2 handright = armBase + new Vector2(+width / 2, +5);
+        Vector2 headBase = position + new Vector2(0, -height + headRadius);
+
+        // Torso
+        Raylib.DrawLineV(legbase, headBase, Color.Black);
+        // Legs
+        Raylib.DrawLineV(legbase, footleft, Color.Black);
+        Raylib.DrawLineV(legbase, footRight, Color.Black);
+        // Arms
+        Raylib.DrawLineV(armBase, handleft, Color.Black);
+        Raylib.DrawLineV(armBase, handright, Color.Black);
+        // Head
+        Raylib.DrawCircleV
--- Asad-Muhammad-a3-game/Program.cs
+++ Asad-Muhammad-a3-game/Program.cs
@@ -1,18 +1,49 @@
 ﻿using Raylib_cs;
 using System.Numerics;
-using static System.Net.Mime.MediaTypeNames;
+using System.Security.Cryptography;
 
 public class Program
 {
-    Player player;
-    Obstacle obstacle;
-    TextReader timesUpText;
+    // If you need variables in the Program class (outside functions), you must mark them as static
+    static string title = "Asad-Muhammad-a3-game"; // Window title
+    static int screenWidth = 800; // Screen width
+    static int screenHeight = 600; // Screen height
+    static int targetFps = 60; // Target frames-per-second
 
-    public Program()
+    static void Main()
     {
-        Raylib.InitWindow(800, 600, "Assignment 3 - 2D Game Project");
+        // Create a window to draw to. The arguments define width and height
+        Raylib.InitWindow(800, 600, "The Collision");
+        // Set the target frames-per-second (FPS)
         Raylib.SetTargetFPS(60);
+        // Setup your game. This is a function YOU define.
+        Setup();
+        // Loop so long as window should not close
+        while (!Raylib.WindowShouldClose())
+        {
+            // Enable drawing to the canvas (window)
+            Raylib.BeginDrawing();
+            // Clear the canvas with one color
+            Raylib.ClearBackground(Color.RayWhite);
+            // Your game code here. This is a function YOU define.
+            Update();
+            // Stop drawing to the canvas, begin displaying the frame
+            Raylib.EndDrawing();
+        }
+        // Close the window
+        Raylib.CloseWindow();
+    }
 
+    static void Setup()
+    {
+        // Your one-time setup code here
         player = new Player();
-        obstacle = new Obstacle();
-        timesUpText = new Text("Times Up, Raylib.GetScreenWidth() /")
+    }
+
+    static void Update()
+    {
+        // Your game code run each frame here
+        player.Move();
+        player.Draw();
+    }
+}
