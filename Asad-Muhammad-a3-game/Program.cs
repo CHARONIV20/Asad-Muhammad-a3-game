380023bfa7de08ab1c5d8c4111a48e2bc826593b
Muhammad Asad <000880751@mohawkcollege.ca>
2024-06-03 09:58:24 -0400

    Add Visual Studio project

---
+++ Asad-Muhammad-a3-game/Program.cs
@@ -0,0 +1,18 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program
+{
+    Player player;
+    Obstacle obstacle;
+    TextReader timesUpText;
+
+    public Program()
+    {
+        Raylib.InitWindow(800, 600, "Assignment 3 - 2D Game Project");
+        Raylib.SetTargetFPS(60);
+
+        player = new Player();
+        obstacle = new Obstacle();
+        timesUpText = new Text("Times Up, Raylib.GetScreenWidth() /")
